4b9d938a0d50442915877dc831b333cc3c49c0d8
Ever Lizarraga <rolank.utn@gmail.com>
2026-06-06 00:42:02 -0300

    Fase 2 - Primer modulo configurado

---
+++ 02-optional/01-optional-basico/src/main/java/io/github/everlizarraga/fundamentos/basico/Main.java
@@ -0,0 +1,10 @@
+package io.github.everlizarraga.fundamentos.basico;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // mvn compile exec:java -pl 01-optional-basico
+    System.out.println("Modulo: 01-optional-basico > Running");
+  }
+
+}
